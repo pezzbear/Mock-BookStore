515a79f5f2b40952c42db5d5461c149e5e85fbd0
Edan Deno <edanzdeno@gmail.com>
2023-10-02 13:37:31 -0500

    Test Commit

--- FinalProject/FinalProject/Controllers/HomeController.cs
+++ FinalProject/FinalProject/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace FinalProject.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page Yuups.";
 
             return View();
         }
