0e2eeaff1e4febbd517137793edc4594135bcfb5
Edan Deno <edanzdeno@gmail.com>
2023-10-06 15:06:02 -0500

    Need to make it so when the user selects from the dropdown the model is sent to the SearchBy controller and the data is filtered and sent back to the BrowseByAuthor

---
+++ FinalProject/FinalProject/Controllers/SearchByController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FinalProject.Models.EntityFramework;
+using FinalProject.Models;
+
+
+namespace FinalProject.Controllers
+{
+    public class SearchByController : Controller
+    {
+
+        HENRY_DATABASEEntities db = new HENRY_DATABASEEntities();
+        // GET: SearchBy
+        public ActionResult SearchByAuthor()
+        {
+            SearchBy model = new SearchBy();
+
+            model.allAuthors = db.AUTHORs.ToList().Select(x => new SelectListItem
+            {
+                Text = x.AUTHOR_FIRST + " " + x.AUTHOR_LAST,
+                Value = x.AUTHOR_NUM.ToString()
+            });
+
+            return PartialView("~/Views/Shared/_SearchByAuthor.cshtml", model);
+        }
+    }
+}
---
+++ FinalProject/FinalProject/Models/SearchBy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FinalProject.Models
+{
+    public class SearchBy
+    {
+        public string selectedAuthor { get; set; }
+
+        public IEnumerable<SelectListItem> allAuthors { get; set; }
+
+        public string selectedPublisher { get; set; }
+
+        public IEnumerable<SelectListItem> allPublisher { get; set; }
+
+        public string selectedLocation { get; set; }
+
+        public IEnumerable<SelectListItem> allLocation { get; set; }
+    }
+}
